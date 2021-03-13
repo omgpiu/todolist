3a116c2db99d8e44f2e30e202318856d2b19000a
omgpiu <dyba@List.ru>
2021-03-13 18:23:52 +0300

    Updates

---
+++ precache-manifest.20e000e84a78678ddc6dafe418fe6b85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647a14eb72b69182b35b9c159e037873",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a18d602635cc24e9401e",
+    "url": "/todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "19757be89a2733664912",
+    "url": "/todolist/static/js/2.9ed81261.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/todolist/static/js/2.9ed81261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18d602635cc24e9401e",
+    "url": "/todolist/static/js/main.525264c9.chunk.js"
+  },
+  {
+    "revision": "5a6a4203af048cd5babd",
+    "url": "/todolist/static/js/runtime-main.85161310.js"
+  }
+]);
